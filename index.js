e40f6de538ea6d80d67c5cf2ea8578f24bc60fe0
Ceci <ceciliadasilveira18@gmail.com>
2022-08-06 18:59:32 -0300

    added background shapes

--- index.js
+++ index.js
@@ -4,46 +4,62 @@
 
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 40;
 
-function toggleContrast() {
-  
-  contrastToggle = !contrastToggle;
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
 
-  if (contrastToggle) {
-    document.body.classList += " dark-theme"
-  } else {
-    document.body.classList.remove("dark-theme")
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${
+      y * boolInt
+    }px) rotate(${y * boolInt}deg)`;
   }
 }
 
-function contact (event) {
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+function contact(event) {
   event.preventDefault();
-  const loading = document.querySelector('.modal__overlay--loading');
-  const success = document.querySelector('.modal__overlay--success');
-  loading.classList += ' modal__overlay--visible';
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
 
   emailjs
     .sendForm(
-      'service_ed4u05q',
-      'template_hsz7g9e',
+      "service_ed4u05q",
+      "template_hsz7g9e",
       event.target,
-      '-KLmlbgHvxWz-50_L'
-    ).then(() => {
-      loading.classList.remove('modal__overlay--visible');
-      success.classList += ' modal__overlay--visible'
-    }).catch(() => {
-      loading.classList.remove('modal__overlay--visible');
-      alert(
-        'The email service is temporarly unavailable. Please contact me directly on ceciliadasilveira18@gmail.com'
-      );
+      "-KLmlbgHvxWz-50_L"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
     })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarly unavailable. Please contact me directly on ceciliadasilveira18@gmail.com"
+      );
+    });
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
   }
-  
-  function toggleModal() {
-    if (isModalOpen) {
-      isModalOpen = false;
-      return document.body.classList.remove('modal--open');    
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open"
-  };
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
